02b8d8afaf605e650aa1c9e28bbbbbb54750e7ce
BugNode0x <sam@bugnode.io>
2024-01-23 17:27:44 -0500

    updates

--- backend/server.js
+++ backend/server.js
@@ -82,7 +82,7 @@ app.get('/auth', (_req, res) => {
     provider: 'authkit',
 
     // The callback endpoint that WorkOS will redirect to after a user authenticates
-    redirectUri: 'http://localhost:3001/callback',
+    redirectUri: 'https://prototype-apidev.noisse.io1/callback',
     clientId,
   });
 
