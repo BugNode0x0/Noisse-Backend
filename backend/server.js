a9a6e244657feec55fe2245747bdf1fcc1601949
sam <149985200+BugNode0x@users.noreply.github.com>
2024-01-25 10:56:07 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,29 +5,16 @@ const { exec } = require('child_process');
 const { createServer } = require('http'); // Ensure this is at the top with other requires
 const { Server } = require('socket.io');
 const axios = require('axios');
-const { WorkOS } = require('@workos-inc/node');
-const cookieParser = require('cookie-parser');
-
-//Javascript Object Signing and Encryption (JOSE)
-// https://www.npmjs.com/package/jose
-const { SignJWT } = require('jose');
-
-// Get secret
-const secret = new Uint8Array(
-  Buffer.from(process.env.JWT_SECRET_KEY, 'base64'),
-);
 
 
 // CONFIG //
 require('dotenv').config(); 
 
 const app = express();
+const authRoutes = require('./auth');
 app.use(express.json());
 app.use(cors());
-app.use(cookieParser());
-
-const workos = new WorkOS(process.env.WORKOS_API_KEY);
-const clientId = process.env.WORKOS_CLIENT_ID;
+app.use('/auth', authRoutes);
 
 const pool = new Pool({
   host: process.env.DB_HOST,
@@ -53,76 +40,9 @@ io.on('connection', (socket) => {
   });
 });
 
-/// AUTHENTICATION ///  
-
-app.get('/auth', (_req, res) => {
-  const authorizationUrl = workos.userManagement.getAuthorizationUrl({
-    // Specify that we'd like AuthKit to handle the authentication flow
-    provider: 'authkit',
-
-    // The callback endpoint that WorkOS will redirect to after a user authenticates
-    redirectUri: 'https://noisse-backend-production.up.railway.app/callback',
-    clientId,
-  });
-
-  // Redirect the user to the AuthKit sign-in page
-  res.redirect(authorizationUrl);
-});
-
-
-app.get('/callback', async (req, res) => {
-  // The authorization code returned by AuthKit
-  const code = req.query.code;
-
-  const { user } = await workos.userManagement.authenticateWithCode({
-    code,
-    clientId,
-  });
-
-  // Create a JWT with the user's information
-  const token = await new SignJWT({
-    // Here you might lookup and retrieve user details from your database
-    user,
-  })
-    .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
-    .setIssuedAt()
-    .setExpirationTime('1h')
-    .sign(secret);
-
-  // Store in a cookie
-  res.cookie('token', token, {
-    path: '/',
-    httpOnly: true,
-    secure: true,
-    sameSite: 'lax',
-  });
-
-  // Use the information in `user` for further business logic.
-
-  // Redirect the user to the homepage
-  res.redirect('/');
-});
-
-app.get('/user', async (req, res) => {
-  const token = req.cookies.token;
-
-  // Verify the JWT signature
-  let verifiedToken;
-  try {
-    verifiedToken = await jwtVerify(token, secret);
-  } catch {
-    res.status(401).send({ isAuthenticated: false });
-  }
-
-  // Return the User object if the token is valid
-  res.status(200).send({
-    isAuthenticated: true,
-    user: verifiedToken.payload.user,
-  });
-});
-
-
 ///
+
+
 
 app.get('/db-check', async (req, res) => {
   try {
