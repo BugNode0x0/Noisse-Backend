09368771533f2d0c576538a585b77bbbd642f415
sam <149985200+BugNode0x@users.noreply.github.com>
2024-01-25 10:56:25 -0500

    Create auth.js

---
+++ backend/auth.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const router = express.Router();
+
+const { WorkOS } = require('@workos-inc/node');
+const cookieParser = require('cookie-parser');
+
+const { SignJWT } = require('jose');
+const { jwtVerify } = require('jose');
+
+
+const secret = new Uint8Array(
+    Buffer.from(process.env.JWT_SECRET_KEY, 'base64'),
+  );
+
+router.use(cookieParser());
+
+
+const workos = new WorkOS(process.env.WORKOS_API_KEY);
+const clientId = process.env.WORKOS_CLIENT_ID;
+
+router.get('/auth', (_req, res) => {
+  const authorizationUrl = workos.userManagement.getAuthorizationUrl({
+    provider: 'authkit',
+
+    redirectUri: 'https://noisse-backend-production.up.railway.app/callback',
+    clientId,
+  });
+
+  // Redirect the user to the AuthKit sign-in page
+  res.redirect(authorizationUrl);
+});
+
+
+router.get('/callback', async (req, res) => {
+
+    const code = req.query.code;
+  
+    const { user } = await workos.userManagement.authenticateWithCode({
+      code,
+      clientId,
+    });
+
+    const token = await new SignJWT({
+        // Here you might lookup and retrieve user details from your database
+        user,
+      })
+        .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
+        .setIssuedAt()
+        .setExpirationTime('1h')
+        .sign(secret);
+    
+      // Store in a cookie
+      res.cookie('token', token, {
+        path: '/',
+        httpOnly: true,
+        secure: true,
+        sameSite: 'lax',
+      });
+
+    res.redirect('/');
+  });
+
+router.get('/user', async (req, res) => {
+    const token = req.cookies.token;
+  
+    // Verify the JWT signature
+    let verifiedToken;
+    try {
+      verifiedToken = await jwtVerify(token, secret);
+    } catch {
+      res.status(401).send({ isAuthenticated: false });
+    }
+  
+    // Return the User object if the token is valid
+    res.status(200).send({
+      isAuthenticated: true,
+      user: verifiedToken.payload.user,
+    });
+  });
+
+module.exports = router;
