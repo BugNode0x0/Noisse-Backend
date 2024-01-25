f7eace9a79e1df45d69a498138073d2ad6c549ed
BugNode0x <sam@bugnode.io>
2024-01-25 14:51:52 -0500

    cors modification url

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ require('dotenv').config();
 
 const app = express();
 const corsOptions = {
-  origin: 'https://noisse-frontend.vercel.app/',
+  origin: 'https://noisse-backend-production.up.railway.app/',
   credentials: true, // to allow sending of cookies
   methods: ['GET', 'POST', /* other HTTP methods you use */]
 };
