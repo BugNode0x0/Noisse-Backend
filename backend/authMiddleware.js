c777a19454c5e4bf4a7045b76af0ff118cb4b7ed
sam <149985200+BugNode0x@users.noreply.github.com>
2024-01-25 11:22:13 -0500

    Create authMiddleware.js

---
+++ backend/authMiddleware.js
@@ -0,0 +1,21 @@
+const { jwtVerify } = require('jose');
+const secret = new Uint8Array(
+    Buffer.from(process.env.JWT_SECRET_KEY, 'base64')
+);
+
+async function authenticateToken(req, res, next) {
+    const token = req.cookies.token; // or req.headers.authorization
+
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided.' });
+    }
+
+    try {
+        await jwtVerify(token, secret);
+        next(); // Token is valid, proceed to the next handler
+    } catch (error) {
+        res.status(403).json({ message: 'Invalid or expired token.' });
+    }
+}
+
+module.exports = authenticateToken;
