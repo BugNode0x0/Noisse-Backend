97b508fa5be9876925bbbcb9b9b82d1bdf44ea50
sam <149985200+BugNode0x@users.noreply.github.com>
2024-01-30 13:08:10 -0500

    implement ac

--- backend/server.js
+++ backend/server.js
@@ -242,6 +242,7 @@ app.get('/active-domains/count', authenticateToken, async (req, res) => {
 
 app.get('/web-domains/count', authenticateToken, async (req, res) => {
   const interval = req.query.interval || 'week';
+  const userId = req.user.id; // Extract user ID from JWT token
 
   let timeRangeCondition;
   switch (interval) {
@@ -255,14 +256,14 @@ app.get('/web-domains/count', authenticateToken, async (req, res) => {
       timeRangeCondition = `timestamp::timestamptz >= NOW() - INTERVAL '30 days'`;
       break;
     default:
-      timeRangeCondition = `timestamp::timestamptz >= NOW() - INTERVAL '7 days'`; // Fallback to 'week' if the interval is not recognized
+      timeRangeCondition = `timestamp::timestamptz >= NOW() - INTERVAL '7 days'`;
       break;
   }
 
-  const query = `SELECT COUNT(*) FROM recon WHERE ${timeRangeCondition}`;
+  const query = `SELECT COUNT(*) FROM recon WHERE ${timeRangeCondition} AND user_id = $1`;
 
   try {
-    const result = await pool.query(query);
+    const result = await pool.query(query, [userId]);
     const count = result.rows[0].count ? parseInt(result.rows[0].count, 10) : 0;
     res.status(200).json({ count });
   } catch (err) {
@@ -270,9 +271,11 @@ app.get('/web-domains/count', authenticateToken, async (req, res) => {
     res.status(500).send('Internal server error');
   }
 });
+
 
 app.get('/assets-ips/count', authenticateToken, async (req, res) => {
   const interval = req.query.interval || 'week';
+  const userId = req.user.id; // Extract user ID from JWT token
 
   let timeRangeCondition;
   switch (interval) {
@@ -286,14 +289,14 @@ app.get('/assets-ips/count', authenticateToken, async (req, res) => {
       timeRangeCondition = `timestamp::timestamptz >= NOW() - INTERVAL '30 days'`;
       break;
     default:
-      timeRangeCondition = `timestamp::timestamptz >= NOW() - INTERVAL '7 days'`; // Fallback to 'week' if the interval is not recognized
+      timeRangeCondition = `timestamp::timestamptz >= NOW() - INTERVAL '7 days'`;
       break;
   }
 
-  const query = `SELECT COUNT(*) FROM dns WHERE ${timeRangeCondition}`;
+  const query = `SELECT COUNT(*) FROM dns WHERE ${timeRangeCondition} AND user_id = $1`;
 
   try {
-    const result = await pool.query(query);
+    const result = await pool.query(query, [userId]);
     const count = result.rows[0].count ? parseInt(result.rows[0].count, 10) : 0;
     res.status(200).json({ count });
   } catch (err) {
@@ -302,8 +305,10 @@ app.get('/assets-ips/count', authenticateToken, async (req, res) => {
   }
 });
 
+
 app.get('/assets-ips/chart-data', authenticateToken, async (req, res) => {
   const interval = req.query.interval || 'week';
+  const userId = req.user.id; // Extract user ID from JWT token
   let timeGroup = 'day'; // Default to grouping by days
 
   // Adjust the timeGroup based on the interval
@@ -323,12 +328,13 @@ app.get('/assets-ips/chart-data', authenticateToken, async (req, res) => {
       DATE_TRUNC('${timeGroup}', timestamp::timestamptz) as period,
       COUNT(*) as count
     FROM dns
+    WHERE user_id = $1
     GROUP BY period
     ORDER BY period
   `;
 
   try {
-    const chartResult = await pool.query(chartQuery);
+    const chartResult = await pool.query(chartQuery, [userId]);
     const chartData = chartResult.rows.map(row => ({
       date: row.period.toISOString(),
       count: row.count
@@ -339,6 +345,7 @@ app.get('/assets-ips/chart-data', authenticateToken, async (req, res) => {
     res.status(500).send('Internal server error');
   }
 });
+
 ///
 
 //  GATHER DOMAINS
@@ -387,16 +394,17 @@ app.get('/active-domains', authenticateToken, async (req, res) => {
   const pageSize = parseInt(req.query.pageSize) || 10;
   const search = req.query.search || ''; // Get the search query parameter
   const offset = (page - 1) * pageSize;
+  const userId = req.user.id; // Extract user ID from JWT token
 
-  // Adjust your SQL query to filter based on the search term using ILIKE for case-insensitive partial matching
-  const baseQuery = `FROM dns WHERE host ILIKE $1`;
+  // Adjust your SQL query to filter based on the search term and user ID
+  const baseQuery = `FROM dns WHERE user_id = $1 AND host ILIKE $2`;
   const countQuery = `SELECT COUNT(*) ${baseQuery}`;
-  const selectQuery = `SELECT host ${baseQuery} ORDER BY host LIMIT $2 OFFSET $3`;
+  const selectQuery = `SELECT host ${baseQuery} ORDER BY host LIMIT $3 OFFSET $4`;
   
   try {
     // Using parameterized queries to prevent SQL injection
-    const countResult = await pool.query(countQuery, [`%${search}%`]);
-    const result = await pool.query(selectQuery, [`%${search}%`, pageSize, offset]);
+    const countResult = await pool.query(countQuery, [userId, `%${search}%`]);
+    const result = await pool.query(selectQuery, [userId, `%${search}%`, pageSize, offset]);
 
     res.status(200).json({
       activeDomains: result.rows,
@@ -410,30 +418,31 @@ app.get('/active-domains', authenticateToken, async (req, res) => {
   }
 });
 
+
 // Get all web URLs
 app.get('/web-domains', authenticateToken, async (req, res) => {
   const page = parseInt(req.query.page) || 1;
   const pageSize = parseInt(req.query.pageSize) || 10;
   const search = req.query.search || '';
   const offset = (page - 1) * pageSize;
+  const userId = req.user.id; // Extract user ID from JWT token
 
-  // Determine if a search term was provided to decide which queries to use.
   let countQuery, selectQuery, queryParams;
   if (search) {
     // If there's a search term, use it as a condition in the WHERE clause.
-    countQuery = `SELECT COUNT(*) FROM recon WHERE url ILIKE $1 OR title ILIKE $1`;
-    selectQuery = `SELECT url, title, status_code FROM recon WHERE url ILIKE $1 OR title ILIKE $1 ORDER BY url LIMIT $2 OFFSET $3`;
-    queryParams = [`%${search}%`, pageSize, offset];
+    countQuery = `SELECT COUNT(*) FROM recon WHERE user_id = $1 AND (url ILIKE $2 OR title ILIKE $2)`;
+    selectQuery = `SELECT url, title, status_code FROM recon WHERE user_id = $1 AND (url ILIKE $2 OR title ILIKE $2) ORDER BY url LIMIT $3 OFFSET $4`;
+    queryParams = [userId, `%${search}%`, pageSize, offset];
   } else {
     // If there's no search term, execute the query without a WHERE clause.
-    countQuery = `SELECT COUNT(*) FROM recon`;
-    selectQuery = `SELECT url, title, status_code FROM recon ORDER BY url LIMIT $1 OFFSET $2`;
-    queryParams = [pageSize, offset];
+    countQuery = `SELECT COUNT(*) FROM recon WHERE user_id = $1`;
+    selectQuery = `SELECT url, title, status_code FROM recon WHERE user_id = $1 ORDER BY url LIMIT $2 OFFSET $3`;
+    queryParams = [userId, pageSize, offset];
   }
   
   try {
     // Execute the queries using the constructed SQL and params.
-    const countResult = await pool.query(countQuery, search ? [`%${search}%`] : []);
+    const countResult = await pool.query(countQuery, search ? [userId, `%${search}%`] : [userId]);
     const selectResult = await pool.query(selectQuery, queryParams);
 
     res.status(200).json({
@@ -447,6 +456,7 @@ app.get('/web-domains', authenticateToken, async (req, res) => {
     res.status(500).send('Internal server error');
   }
 });
+
 
 // 
 app.get('/assets-ips', authenticateToken, async (req, res) => {
@@ -454,28 +464,28 @@ app.get('/assets-ips', authenticateToken, async (req, res) => {
   const pageSize = parseInt(req.query.pageSize) || 10;
   const search = req.query.search || '';
   const offset = (page - 1) * pageSize;
+  const userId = req.user.id; // Extract user ID from JWT token
 
-  // Determine if a search term was provided to decide which queries to use.
   let countQuery, selectQuery, queryParams;
   if (search) {
     // If there's a search term, use it as a condition in the WHERE clause.
-    countQuery = `SELECT COUNT(*) FROM dns WHERE a ILIKE $1 OR host ILIKE $1`;
-    selectQuery = `SELECT a, host, status_code FROM dns WHERE a ILIKE $1 OR host ILIKE $1 ORDER BY a LIMIT $2 OFFSET $3`;
-    queryParams = [`%${search}%`, pageSize, offset];
+    countQuery = `SELECT COUNT(*) FROM dns WHERE user_id = $1 AND (a ILIKE $2 OR host ILIKE $2)`;
+    selectQuery = `SELECT a, host, status_code FROM dns WHERE user_id = $1 AND (a ILIKE $2 OR host ILIKE $2) ORDER BY a LIMIT $3 OFFSET $4`;
+    queryParams = [userId, `%${search}%`, pageSize, offset];
   } else {
-    // If there's no search term, execute the query without a WHERE clause.
-    countQuery = `SELECT COUNT(*) FROM recon`;
-    selectQuery = `SELECT a, host, status_code FROM dns ORDER BY a LIMIT $1 OFFSET $2`;
-    queryParams = [pageSize, offset];
+    // If there's no search term, execute the query with the user_id filter.
+    countQuery = `SELECT COUNT(*) FROM dns WHERE user_id = $1`;
+    selectQuery = `SELECT a, host, status_code FROM dns WHERE user_id = $1 ORDER BY a LIMIT $2 OFFSET $3`;
+    queryParams = [userId, pageSize, offset];
   }
   
   try {
     // Execute the queries using the constructed SQL and params.
-    const countResult = await pool.query(countQuery, search ? [`%${search}%`] : []);
+    const countResult = await pool.query(countQuery, search ? [userId, `%${search}%`] : [userId]);
     const selectResult = await pool.query(selectQuery, queryParams);
 
     res.status(200).json({
-      webDomains: selectResult.rows,
+      assetsIps: selectResult.rows,
       total: parseInt(countResult.rows[0].count),
       page,
       pageSize
@@ -485,34 +495,35 @@ app.get('/assets-ips', authenticateToken, async (req, res) => {
     res.status(500).send('Internal server error');
   }
 });
+
 
 app.get('/flaws', authenticateToken, async (req, res) => {
   const page = parseInt(req.query.page) || 1;
   const pageSize = parseInt(req.query.pageSize) || 10;
   const search = req.query.search || '';
   const offset = (page - 1) * pageSize;
+  const userId = req.user.id; // Extract user ID from JWT token
 
-  // Determine if a search term was provided to decide which queries to use.
   let countQuery, selectQuery, queryParams;
   if (search) {
     // If there's a search term, use it as a condition in the WHERE clause.
-    countQuery = `SELECT COUNT(*) FROM threats WHERE matched_at ILIKE $1 OR severity ILIKE $1`;
-    selectQuery = `SELECT matched_at, name, severity, template_id FROM threats WHERE matched_at ILIKE $1 OR name ILIKE $1 ORDER BY matched_at LIMIT $2 OFFSET $3;    `;
-    queryParams = [`%${search}%`, pageSize, offset];
+    countQuery = `SELECT COUNT(*) FROM threats WHERE user_id = $1 AND (matched_at ILIKE $2 OR name ILIKE $2)`;
+    selectQuery = `SELECT matched_at, name, severity, template_id FROM threats WHERE user_id = $1 AND (matched_at ILIKE $2 OR name ILIKE $2) ORDER BY matched_at LIMIT $3 OFFSET $4;`;
+    queryParams = [userId, `%${search}%`, pageSize, offset];
   } else {
-    // If there's no search term, execute the query without a WHERE clause.
-    countQuery = `SELECT COUNT(*) FROM threats`;
-    selectQuery = `SELECT matched_at, name, severity, template_id FROM threats ORDER BY matched_at LIMIT $1 OFFSET $2`;
-    queryParams = [pageSize, offset];
+    // If there's no search term, execute the query with the user_id filter.
+    countQuery = `SELECT COUNT(*) FROM threats WHERE user_id = $1`;
+    selectQuery = `SELECT matched_at, name, severity, template_id FROM threats WHERE user_id = $1 ORDER BY matched_at LIMIT $2 OFFSET $3`;
+    queryParams = [userId, pageSize, offset];
   }
   
   try {
     // Execute the queries using the constructed SQL and params.
-    const countResult = await pool.query(countQuery, search ? [`%${search}%`] : []);
+    const countResult = await pool.query(countQuery, search ? [userId, `%${search}%`] : [userId]);
     const selectResult = await pool.query(selectQuery, queryParams);
 
     res.status(200).json({
-      webDomains: selectResult.rows,
+      threats: selectResult.rows,
       total: parseInt(countResult.rows[0].count),
       page,
       pageSize
@@ -522,6 +533,7 @@ app.get('/flaws', authenticateToken, async (req, res) => {
     res.status(500).send('Internal server error');
   }
 });
+
 
 
 
