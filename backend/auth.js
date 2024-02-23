5475fdd95340267032ca7a8d811eac3fd7bbb9a7
BugNode0x <sam@bugnode.io>
2024-02-23 00:27:32 -0500

    early new user debug

--- backend/auth.js
+++ backend/auth.js
@@ -56,62 +56,31 @@ router.get('/auth', (_req, res) => {
 
 
 router.get('/callback', async (req, res) => {
+
     const code = req.query.code;
+  
+    const { user } = await workos.userManagement.authenticateWithCode({
+      code,
+      clientId,
+    });
+
+    const token = await new SignJWT({
+        user,
+      })
+        .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
+        .setIssuedAt()
+        .setExpirationTime('1h')
+        .sign(secret);
     
-    try {
-        const { user } = await workos.userManagement.authenticateWithCode({
-            code,
-            clientId,
-        });
-  
-        // Retrieve the corresponding user_id using hunter_id from 'users' table
-        const userQuery = 'SELECT user_id FROM users WHERE hunter_id = $1';
-        const userResult = await pool.query(userQuery, [user.id]);
-        const userId = userResult.rows[0]?.user_id;
-  
-        if (!userId) {
-            return res.status(404).send('User not found');
-        }
-  
-        const userCheckQuery = 'SELECT stripe_customer_id FROM user_payments WHERE user_id = $1';
-        const userCheckResult = await pool.query(userCheckQuery, [userId]);
-        let stripeCustomerId = userCheckResult.rows.length > 0 ? userCheckResult.rows[0].stripe_customer_id : null;
-  
-        if (!stripeCustomerId) {
-            const stripeCustomer = await stripe.customers.create({
-                email: user.email, // or any other identifier you get from WorkOS
-            });
-            stripeCustomerId = stripeCustomer.id;
-  
-            const dbUserQuery = `
-                INSERT INTO user_payments (user_id, stripe_customer_id, subscription_status)
-                VALUES ($1, $2, 'pending') ON CONFLICT (user_id) 
-                DO UPDATE SET stripe_customer_id = EXCLUDED.stripe_customer_id;
-            `;
-            await pool.query(dbUserQuery, [userId, stripeCustomerId]);
-        }
-  
-        // Create JWT token and set cookie
-        const token = await new SignJWT({
-            user,
-        })
-            .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
-            .setIssuedAt()
-            .setExpirationTime('1h')
-            .sign(secret);
-  
-        res.cookie('token', token, {
-            path: '/',
-            httpOnly: true,
-            secure: true,
-            sameSite: 'None',
-        });
-  
-        res.redirect('https://dev-noisse.vercel.app');
-    } catch (error) {
-        console.error('Error in /callback:', error);
-        res.status(500).send('Internal server error');
-    }
+      // Store in a cookie
+      res.cookie('token', token, {
+        path: '/',
+        httpOnly: true,
+        secure: true,
+        sameSite: 'None',
+      });
+
+    res.redirect('https://dev-noisse.vercel.app');
   });
 
 
