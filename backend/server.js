1fb4c53fc7d89daf0982c506dfa15886b4c2187b
BugNode0x <sam@bugnode.io>
2024-03-21 11:33:21 -0400

    adding stuff

--- backend/server.js
+++ backend/server.js
@@ -1081,7 +1081,7 @@ app.post('/user/webhook', authenticateToken, async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || port;
   httpServer.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
     io.attach(httpServer);
