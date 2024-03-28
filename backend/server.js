a8235fca24db4d57f80fd11c8ea1be041466b6a8
BugNode0x <sam@bugnode.io>
2024-03-28 15:52:56 -0400

    cope

--- backend/server.js
+++ backend/server.js
@@ -1192,5 +1192,4 @@ app.post('/user/webhook', authenticateToken, async (req, res) => {
 const PORT = process.env.PORT || 3001;
   httpServer.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-    io.attach(httpServer);
   });
